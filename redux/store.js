e6af55ebb7656b86aab4aa9bfeadba56b6e75e13
bhuezodd <huezo.dd@gmail.com>
2021-06-04 14:28:35 -0600

    fixed redux persist

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from 'redux'
 import {persistStore, persistReducer} from 'redux-persist'
 
-import storage from 'redux-persist/lib/storage'
+import storage from './storage'
 
 const persistConfig = {
   key: 'root',
